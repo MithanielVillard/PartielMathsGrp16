cf3775098c7aee358415371a1355df2ad0d6f7fd
MithanielVillard <mvillardhernandez@gaming.tech>
2024-12-24 13:53:44 +0100

    [ADD] Click detection and point

--- Exercice3.py
+++ Exercice3.py
@@ -1,27 +1,30 @@
 import customtkinter as ctk
-
-def on_click(event):
-    if event.inaxes:
-        print(f'data coords {event.xdata} {event.ydata},',
-              f'pixel coords {event.x} {event.y}')
+from Graph import Graph
 
 ctk.set_appearance_mode("dark")
 
 root = ctk.CTk()
 root.title("Curve Generator")
 root.geometry("1200x800")
-root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
 root.configure(fg_color = "#222f3e")
 root.update()
 
 #-------------------------------------------
 settings = ctk.CTkFrame(master=root, fg_color="#576574")
-settings.grid(row=0, column=1, padx=(10, 20), pady=20, sticky="nsew")
+settings.grid(row=0, column=1, padx=(5, 10), pady=10, sticky="nsew")
 
 plotframe = ctk.CTkFrame(master=root, fg_color="#576574")
-plotframe.grid(row=0, column=0, padx=(20, 10), pady=20, sticky="nsew")
+plotframe.grid(row=0, column=0, padx=(10, 5), pady=10, sticky="nsew")
 plotframe.columnconfigure(0, weight=1)
-plotframe.rowconfigure(0, weight=1)
+plotframe.rowconfigure(1, weight=2)
+
+toolbar = ctk.CTkFrame(master=plotframe, fg_color="#222f3e", height=50)
+toolbar.grid(row=0, column=0, padx=10, pady=(10, 5), sticky="ew")
+
+graph = Graph(plotframe)
+graph.update()
+#----------------------------------------------
 
 root.mainloop()
--- Graph.py
+++ Graph.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.patches import FancyBboxPatch
 
 class Graph:
     def __init__(self, plotframe):
-        fig, ax = plt.subplots()
+        self.fig, self.ax = plt.subplots()
 
-        plt.connect('button_press_event', on_click)
+        plt.connect('button_press_event', self.on_click)
 
-        ax.set_title("Curve Generator", fontsize=20, pad=20, color="white")
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-        fig.patch.set_facecolor("white")
-        ax.grid(True)
+        self.ax.set_title("Curve Generator", fontsize=20, pad=20, color="white")
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set(xlim=(-1, 1), ylim=(-1, 1))
+        self.fig.patch.set_facecolor("#222f3e")
+        self.ax.grid(True)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=plotframe)
+        self.canvas.get_tk_widget().grid(column=0, row=1, pady=10, padx=10, sticky="nsew")
 
-        ax.scatter(4, 4, color="r")
+        self.canvas.draw()
 
-        canvas = FigureCanvasTkAgg(fig, master=plotframe)
-        canvas.get_tk_widget().grid(column=0, row=0, pady=5, padx=5, sticky="nsew")
+    def update(self):
+        self.canvas.draw()
 
-        canvas.draw()
+    def on_click(self, event):
+        if event.inaxes:
+            self.ax.scatter(event.xdata, event.ydata, color="r")
+            self.update()
+            print(f'data coords {event.xdata} {event.ydata},',
+                  f'pixel coords {event.x} {event.y}')
